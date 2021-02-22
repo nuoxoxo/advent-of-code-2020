d06e9582a4a8437e1545cb795fbcdf230d3c8fc6
malcolmnuoxu <49533950+malcolmnuoxu@users.noreply.github.com>
2021-02-22 14:40:33 +0100

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,18 +1,18 @@
-grid = []
+plan = []
 
 with open ('3.txt', 'r') as file:
     for line in file:
         # plan.append(line.split()) # add not string but sublist!
-        grid.extend(line.split())
+        plan.extend(line.split())
 
 """Part 1"""
 # Record 1
 counter = x = y = 0
-while x < len(grid):
-    if grid[x][y] == '#':
+while x < len(plan):
+    if plan[x][y] == '#':
         counter += 1
     x += 1
-    if y + 3 > len(grid[0]) - 1:
+    if y + 3 > len(plan[0]) - 1:
         y = y + 3 - 31
     else:
         y += 3
@@ -22,11 +22,11 @@ rec1 = counter
 """Part 2"""
 # Record 2
 counter = x = y = 0
-while x < len(grid):
-    if grid[x][y] == '#':
+while x < len(plan):
+    if plan[x][y] == '#':
         counter += 1
     x += 1
-    if y + 1 > len(grid[0]) - 1:
+    if y + 1 > len(plan[0]) - 1:
         y = y + 1 - 31
     else:
         y += 1
@@ -35,11 +35,11 @@ rec2 = counter
 
 # Record 3
 counter = x = y = 0
-while x < len(grid):
-    if grid[x][y] == '#':
+while x < len(plan):
+    if plan[x][y] == '#':
         counter += 1
     x += 1
-    if y + 5 > len(grid[0]) - 1:
+    if y + 5 > len(plan[0]) - 1:
         y = y + 5 - 31
     else:
         y += 5
@@ -48,11 +48,11 @@ rec3 = counter
 
 # Record 4
 counter = x = y = 0
-while x < len(grid):
-    if grid[x][y] == '#':
+while x < len(plan):
+    if plan[x][y] == '#':
         counter += 1
     x += 1
-    if y + 7 > len(grid[0]) - 1:
+    if y + 7 > len(plan[0]) - 1:
         y = y + 7 - 31
     else:
         y += 7
@@ -61,11 +61,11 @@ rec4 = counter
 
 # Record 5
 counter = x = y = 0
-while x < len(grid):
-    if grid[x][y] == '#':
+while x < len(plan):
+    if plan[x][y] == '#':
         counter += 1
     x += 2
-    if y + 1 > len(grid[0]) - 1:
+    if y + 1 > len(plan[0]) - 1:
         y = y + 1 - 31
     else:
         y += 1
