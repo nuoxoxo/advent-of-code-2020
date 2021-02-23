475c93de04c65bec8f8a379e5d7080e603fd53fd
malcolmnuoxu <49533950+malcolmnuoxu@users.noreply.github.com>
2021-02-23 11:56:05 +0100

    Update day6ver0.py

--- day6ver0.py
+++ day6ver0.py
@@ -1,51 +1,37 @@
-from collections import Counter
-from itertools import chain
-
 file = open('6.txt', 'r')
-team = [x.split() for x in file.readlines()]
+team = [x.strip() for x in file.readlines()]
 # above line: .split -> list of lists ; .strip -> list of strings
+
+### part 1
 
 r = -1
 for x in range(len(team)):
     r += 1
-    if team[x] == []:
+    if team[x] == '':
         for y in range(x-r+1, x):
             team[x-r] += team[y]
-            team[y] = []
+            team[y] = ''
         r = 0
     if x == len(team) - 1:
         for y in range(x-r+1, len(team)):
             team[x-r] += team[y]
-            team[y] = []
-
+            team[y] = ''
 # then the list looks like:
-#n'], [], [], ['jngdhxfesqzwcptlmvb', 'tlpyhvnoxsagwmqrdzcue'], [], []]
+#'juqckrlojlhsyfcn', '', '', 'jngdhxfesqzwcptlmvbtlpyhvnoxsagwmqrdzcue', '', '']
 
 team = list(filter(None, team))
-
 # after filter empty strs:
-#['juqckrl', 'ojlhsyfcn'], ['jngdhxfesqzwcptlmvb', 'tlpyhvnoxsagwmqrdzcue']]
+#wfzghucjvs', 'juqckrlojlhsyfcn', 'jngdhxfesqzwcptlmvbtlpyhvnoxsagwmqrdzcue']
 
-""" part 1 """
+def yess(s):
+    find_uniq_char = ''.join(set(s))
+    leng = len(find_uniq_char)
+    return leng
 
-n = 0
-
+count = 0
 for x in range(len(team)):
-    temp = (set(line) for line in team[x])
-    cntr = Counter(chain.from_iterable(temp))
-    lett = [chr for chr, count in cntr.items()]
-    n += len(lett)
+    w = yess(team[x])
+    count += w
 
-print(n)
-
-""" part 2 """
-
-n = 0
-
-for x in range(len(team)):
-    temp = (set(line) for line in team[x])
-    cntr = Counter(chain.from_iterable(temp))
-    lett = [chr for chr, count in cntr.items() if count == len(team[x])]
-    n += len(lett)
-
-print(n)
+# print(team)
+print(count)
