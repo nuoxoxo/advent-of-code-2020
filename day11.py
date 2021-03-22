ea20514c00029f2a77acebc95a2d025316ef96a8
malcolmnuoxu <49533950+malcolmnuoxu@users.noreply.github.com>
2021-03-22 13:06:29 +0100

    ...brute forced it

--- day11.py
+++ day11.py
@@ -1,3 +1,8 @@
+### variation on conway's game
+### deepcopy cf. advent19 day02
+
+import copy
+
 a = []
 with open('11.txt') as f:
     for x in f.readlines():
@@ -15,6 +20,8 @@ for x in a:
             tmp.append(1)
     mapping.append(tmp)
 
+### top and bottom frame
+
 top_and_bottom_row = []
 for x in mapping[0]:
     top_and_bottom_row.append(-1)
@@ -22,10 +29,7 @@ for x in mapping[0]:
 mapping.insert(0, top_and_bottom_row)
 mapping.append(top_and_bottom_row)
 
-### top and bottom framing done
-
-# for x in mapping:
-#     print(x)
+### left and right frame
 
 mapping[0].insert(0, -1)
 mapping[len(mapping) - 1].insert(0, -1)
@@ -37,92 +41,155 @@ for x in range(1, len(mapping) - 1):
     mapping[x].append(-1)
 
 
-
-
-for x in range(0, len(mapping)):
-    for y in range(0, len(mapping[x])):
-        if mapping[x][y] == 0:
-            mapping[x][y] = 1
-
-# for x in mapping:
-#     print(x)
-
 #### Main quest
-# empt (L): if no occupied adjacent seats, L --> occupied
-# occu (#): if 4 or + seats occupied adjacent, # -> empty
-# flor (.): static
 
+############################
+##                        ##
+##         Part 1         ##
+##                        ##
+############################
 
-# def check_neighbors(i, j):
+# # empt (L): if no occupied adjacent seats, L --> occupied
+# # occu (#): if 4 or + seats occupied adjacent, # -> empty
+# # flor (.): static
+
+# def check_neighbors(x, y):
 #     count_hash = 0
-#     for x in range(i - 1, i + 2):
-#         for y in range(j - 1, j + 2):
-#             if x != i and y != j:
-#                 if a[x][y] == '#':
+#     for i in range(x - 1, x + 2):
+#         for j in range(y - 1, y + 2):
+#             # if i != x and j != y:
+#             if (i, j) != (x, y):
+#                 if mapping[i][j] == 1:
 #                     count_hash += 1
 #     return count_hash
 
-def check_neighbors(i, j):
-    count_hash = 0
-    for x in range(i - 1, i + 2):
-        for y in range(j - 1, j + 2):
-            if x != i and y != j:
-                if mapping[x][y] == 1:
-                    count_hash += 1
+# maptemp = copy.deepcopy(mapping)
 
+# counter = -1
+# while True:
+#     counter = 0
+#     for x in range(1, len(maptemp) - 1):
+#         for y in range(1, len(maptemp[x]) - 1):
+#             if mapping[x][y] == 0:
+#                 # print(check_neighbors(x, y))
+#                 if check_neighbors(x, y) == 0:
+#                     # mapping[x][y] = 1
+#                     maptemp[x][y] = 1
+#                     counter += 1
+#             elif mapping[x][y] == 1:
+#                 if check_neighbors(x, y) >= 4:
+#                     # mapping[x][y] = 0
+#                     maptemp[x][y] = 0
+#                     counter += 1
+#     if counter == 0:
+#         break
+#     mapping = copy.deepcopy(maptemp)
+
+
+# n = 0
+# for x in range(0, len(mapping)):
+#     for y in range(0, len(mapping[0])):
+#         if mapping[x][y] == 1:
+#             n += 1
+# print(n)
+
+
+############################
+##                        ##
+##         Part 2         ##
+##                        ##
+############################
+
+import time
+
+maptemp = copy.deepcopy(mapping)
+
+def check_visible(x, y):
+    count_hash = 0
+    i = 0   ### nw
+    while x - i >= 0 and y - i >= 0:
+        i += 1
+        if mapping[x - i][y - i] == 0:
+            break
+        elif mapping[x - i][y - i] == 1:
+            count_hash += 1
+            break
+    i = 0   ### north
+    while x - i >= 0:
+        i += 1
+        if mapping[x - i][y] == 0:
+            break
+        elif mapping[x - i][y] == 1:
+            count_hash += 1
+            break
+    i = 0   ### ne
+    while x - i >= 0 and y + i < len(mapping[x]) - 1:
+        i += 1
+        if mapping[x - i][y + i] == 0:
+            break
+        elif mapping[x - i][y + i] == 1:
+            count_hash += 1
+            break
+    i = 0   ### w
+    while y - i >= 0:
+        i += 1
+        if mapping[x][y - i] == 0:
+            break
+        elif mapping[x][y - i] == 1:
+            count_hash += 1
+            break
+    i = 0   ### e
+    while y + i < len(mapping[x]) - 1:
+        i += 1
+        if mapping[x][y + i] == 0:
+            break
+        elif mapping[x][y + i] == 1:
+            count_hash += 1
+            break
+    i = 0   ### sw
+    while x + i < len(mapping) - 1 and y - i >= 0:
+        i += 1
+        if mapping[x + i][y - i] == 0:
+            break
+        elif mapping[x + i][y - i] == 1:
+            count_hash += 1
+            break
+    i = 0   ### s
+    while x + i < len(mapping) - 1:
+        i += 1
+        if mapping[x + i][y] == 0:
+            break
+        elif mapping[x + i][y] == 1:
+            count_hash += 1
+            break
+    i = 0   ### se
+    while x + i < len(mapping) - 1 and y + i < len(mapping[x]) - 1:
+        i += 1
+        if mapping[x + i][y + i] == 0:
+            break
+        elif mapping[x + i][y + i] == 1:
+            count_hash += 1
+            break
     return count_hash
 
-# empt (L): if no occupied adjacent seats, L --> occupied
-# occu (#): if 4 or + seats occupied adjacent, # -> empty
-# flor (.): static
+tic = time.perf_counter()
 
 counter = -1
-while counter != 0:
+while True:
     counter = 0
-    for x in range(1, len(mapping) - 1):
-        for y in range(1, len(mapping[x]) - 1):
+    for x in range(1, len(maptemp) - 1):
+        for y in range(1, len(maptemp[x]) - 1):
             if mapping[x][y] == 0:
-                if check_neighbors(x, y) == 0:
-                    mapping[x][y] = 1
+                if check_visible(x, y) == 0:
+                    maptemp[x][y] = 1
                     counter += 1
             elif mapping[x][y] == 1:
-                if check_neighbors(x, y) >= 4:
-                    mapping[x][y] = 0
+                if check_visible(x, y) >= 5:
+                    maptemp[x][y] = 0
                     counter += 1
-    for x in mapping:
-        print(x)
-    print(counter)
-
-##################################
-####### maybe can't use a loop
-####### because looping has sequence/order
-####### in game of life changes are simultaneous
-##################################
-
-### problem:
-### str is immutable (('str' object does not support item assignment))
-### solution:
-### create an array of int, L:0, #:1, .:-1
-
-# mapping = []
-# for x in a:
-#     tmp = []
-#     for y in a:
-#         if y == '.' or y == ' ':
-#             tmp.append(-1)
-#         elif y == 'L':
-#             tmp.append(0)
-#         elif y == '#':
-#             tmp.append(1)
-#     mapping.append(tmp)
-# print(len(mapping[0]))
-# print(len(mapping))
-# print(mapping)
-
-
-# print(len(mapping))
-# for x in mapping:
-#     print(len(x))
+    if counter == 0:
+        break
+    mapping = copy.deepcopy(maptemp)
 
 n = 0
 for x in range(0, len(mapping)):
@@ -130,4 +197,9 @@ for x in range(0, len(mapping)):
         if mapping[x][y] == 1:
             n += 1
 print(n)
-### too hi: 7251, 3598
+
+toc = time.perf_counter()
+
+print(toc - tic)
+
+### elapse: 5s
