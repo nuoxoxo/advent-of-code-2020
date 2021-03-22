0f9d660c401c285f74223abc0267684d37c1dc7a
malcolmnuoxu <49533950+malcolmnuoxu@users.noreply.github.com>
2021-03-22 01:33:26 +0100

    Create day01.py

---
+++ day01.py
@@ -0,0 +1,23 @@
+file = open('1.txt')
+
+# part 1
+content = file.read()
+li = content.split()
+for x in range(len(li)):
+    li[x] = int(li[x])
+for x in range(len(li)):
+    for y in range(x + 1, len(li)):
+        if li[x] + li[y] == 2020:
+            mult = li[x] * li[y]
+            print(mult)
+
+# part 2
+for x in range(len(li)):
+    li[x] = int(li[x])
+for x in range(len(li)):
+    for y in range(x + 1, len(li)):
+        for z in range(y + 1, len(li)):
+            if li[x] + li[y] + li[z] == 2020:
+                mult = li[x] * li[y] * li[z]
+                print(mult)
+                
