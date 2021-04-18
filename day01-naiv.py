7747179bdd47d28d77df7abf05ab88859c1cddd6
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2021-04-18 14:42:00 +0200

    Update day01-naiv.py

--- day01-naiv.py
+++ day01-naiv.py
@@ -1,22 +1,24 @@
 file = open('1.txt')
 
-# part 1  :::  Time complexity O(Ncube)
 content = file.read()
 li = content.split()
+
 for x in range(len(li)):
     li[x] = int(li[x])
+
+# part 1  :::  Time complexity O(N cube)
 for x in range(len(li)):
     for y in range(x + 1, len(li)):
+        
         if li[x] + li[y] == 2020:
             mult = li[x] * li[y]
             print(mult)
 
 # part 2
 for x in range(len(li)):
-    li[x] = int(li[x])
-for x in range(len(li)):
     for y in range(x + 1, len(li)):
         for z in range(y + 1, len(li)):
+            
             if li[x] + li[y] + li[z] == 2020:
                 mult = li[x] * li[y] * li[z]
                 print(mult)
