9b98b960e336c7f6f1ff69a221c85ba3831228f3
nuo <49533950+nuoxoxo@users.noreply.github.com>
2021-04-20 00:54:33 +0200

    Create day01-adv2.js

---
+++ day01-adv2.js
@@ -0,0 +1,71 @@
+// O(n) constant time lookup
+
+const fs = require('fs');
+
+// Part 1
+
+const lines = fs.readFileSync('2001.txt').toString().split('\n')
+const numberArr = lines.map(Number);
+const numberSet = new Set();
+
+
+for (let number of numberArr)
+{
+  const diff = 2020 - number;
+  if (numberSet.has(diff))
+  {
+    console.log(number * diff);
+  }
+  numberSet.add(number);
+}
+
+function partOne() => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// Part 2
+
+const pairsum = {};
+
+for (let i = 0; i < numberArr.length; i++)
+{
+  for (let j = i + 1; j < numberArr.length; j++)
+  {
+    const sum = numberArr[i] + numberArr[j];
+    pairsum[sum] = [numberArr[i], numberArr[j]];
+  }
+}
+
+for (let number of numberArr)
+{
+  const diff = 2020 - number;
+  if (diff in pairsum)
+  {
+    let [ a, b ] = pairsum[diff];
+    console.log(number * a * b);
+    break;
+  }
+}
+
+// 
+// "js object" revision
+//
+// obj = {name: "my name", age: 45}
+//
+// 'name' in obj
+// > true
+//
+// 'age' in obj
+// > true
+//
+
