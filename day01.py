427a355279811e22e2e0947a04b48e8d47f9364e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2021-06-04 20:41:33 +0200

    day01fixpy

--- day01.py
+++ day01.py
@@ -6,10 +6,9 @@ li = content.split()
 for x in range(len(li)):
     li[x] = int(li[x])
 
-# part 1  :::  Time complexity O(N cube)
+# part 1::: Time complexity O(N cube)
 for x in range(len(li)):
     for y in range(x + 1, len(li)):
-        
         if li[x] + li[y] == 2020:
             mult = li[x] * li[y]
             print(mult)
@@ -18,8 +17,6 @@ for x in range(len(li)):
 for x in range(len(li)):
     for y in range(x + 1, len(li)):
         for z in range(y + 1, len(li)):
-            
             if li[x] + li[y] + li[z] == 2020:
                 mult = li[x] * li[y] * li[z]
                 print(mult)
-                
